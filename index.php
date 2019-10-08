47adc8d303f5884c847351a578830784e28786ac
kumarvishal7 <54521342+kumarvishal7@users.noreply.github.com>
2019-10-08 19:11:55 +0530

    Added Link to Heroku

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ echo "<h1>My Name is: $name </h1>";
 <button type="button" class="btn btn-dark" >CLICK ME!</button>
 <button type="button" class="btn btn-primary" >CLICK ME!</button>
 <button type="button" class="btn btn-success" >CLICK ME!</button>
+<a href="https://www.heroku.com" target="" class="btn btn-danger">Heroku.com</a>
 
 <?php
 require 'includes/footer.php' 
